884349bdef9289e86cac3f29207355f23407fb84
Kamayani Rai <87941109+KamayaniR@users.noreply.github.com>
2025-10-10 15:24:20 -0700

    Time: 4 ms (96.86%), Space: 31.4 MB (90.81%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,6 +1,4 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        distinct_num = set(nums)
-        return len(nums) != len(distinct_num)
-        
-        
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        unique_num = set(nums)
+        return len(nums) != len(unique_num)
