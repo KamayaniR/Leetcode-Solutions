acdb7893c2ab074db73dcf037e156488dbccb3c5
Kamayani Rai <87941109+KamayaniR@users.noreply.github.com>
2026-05-03 19:37:06 -0700

    Time: 0 ms (100%), Space: 20.7 MB (28.15%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -3,12 +3,6 @@ class Solution:
 
         if nums and nums[0] <= target <= nums[~0]:
             left, right = bisect_left(nums,target), bisect_right(nums,target)
-            
             if left < right:
-                 return[left, right-1]
+                return [left, right -1]
         return [-1,-1]
-        
-
-
-         
-        
